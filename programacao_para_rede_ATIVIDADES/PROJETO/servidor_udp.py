44b4ce6dbbd01f12adb925fc012670cd6b466daf
MoabIFRN <149834580+MoabIFRN@users.noreply.github.com>
2023-11-24 23:04:17 -0300

    Update servidor_udp.py

--- programacao_para_rede_ATIVIDADES/PROJETO/servidor_udp.py
+++ programacao_para_rede_ATIVIDADES/PROJETO/servidor_udp.py
@@ -1,24 +1,30 @@
 import socket
+HOST_SERVER = 'localhost'   # Definindo o IP do servidor
+SOCKET_PORT = 50000         # Definindo a porta
+BUFFER_SIZE = 512           # Definindo o tamanho do buffer
+CODE_PAGE   = 'utf-8'       # Definindo a página de códigos de caracteres-
+
+MAX_LISTEN  = 1             # Definindo o máximo de conexões enfileiradas 
 
 # Criação do socket, IPv4 e UDP
 socket_servidor = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # IP e porta associados ao servidor
-IP_PORTA = ("0.0.0.0", 5000)
+IP_PORTA = (HOST_SERVER, SOCKET_PORT )
 socket_servidor.bind(IP_PORTA)
 
 # Recebendo os dados
 print("Esperando dados do cliente...")
-dados, endereco_cliente = socket_servidor.recvfrom(1024)
+dados, endereco_cliente = socket_servidor.recvfrom(BUFFER_SIZE)
 
 # Decodifica os dados
-texto = dados.decode()
+texto = dados.decode(CODE_PAGE)
 
 # Mostra os dados
 print(f"Dados recebidos de {endereco_cliente}: {texto}")
 
 # Codifica e envia a resposta
-dados = "Obrigado pelos dados!".encode()
+dados = "Obrigado pelos dados!".encode(CODE_PAGE)
 socket_servidor.sendto(dados, endereco_cliente)
 
 # Libera os recursos do socket
